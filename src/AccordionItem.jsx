ba2ca87452f3be35a4243e5aba7fd445621d7bd3
Hedita <hedi.takrami@gmail.com>
2023-03-05 16:19:27 +0330

    refactor the code

---
+++ src/AccordionItem.jsx
@@ -0,0 +1,25 @@
+import React, { useState, Fragment } from "react";
+import "./scss/index.scss";
+
+function AccordionItem({ title, body, id }) {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleActivity() {
+    setIsActive(!isActive);
+  }
+
+  return (
+    <Fragment key={id}>
+      <div className="accordion">
+        <div className="accordion-title" onClick={handleActivity}>
+          {title}
+          <div>{isActive ? "▴" : "▼"}</div>
+        </div>
+        {isActive && <div className="accordion-content">{body}</div>}
+        <hr />
+      </div>
+    </Fragment>
+  );
+}
+
+export default AccordionItem;
